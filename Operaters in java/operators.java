6472a3bffefb96e8f5b762315716283e570a24c3
Owais Ahmad <146345893+owais2004@users.noreply.github.com>
2024-05-05 10:56:46 +0500

    Add files via upload

---
+++ Operaters in java/operators.java
@@ -0,0 +1,20 @@
+public class CWH_ch2_08_Operators {
+  public static void main(String[] args) {
+      //Arithmetic Operators(+,-,*,/,&)
+      int  a = 20;
+      int b = a % 6; //Modulo operator
+      // 4.8%1.1 --> returns decimal remainder
+      System.out.println(b);
+      //Assignment Operators(=,+=)
+       b += 3;
+      System.out.println(b);
+      //Comparison Operators(==,>=,<=)
+      //logical Operators(&&,||,!)
+      //Bitwise Operators(&)
+      System.out.println(2&2);
+//        10     2
+//        11     3
+//     ------    +
+//        10     2
+  }
+}
